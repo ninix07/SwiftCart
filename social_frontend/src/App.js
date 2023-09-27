884bb2b1c9a90c01d9cf71eeb790d59f2f483b0a
Nigam <nigam21nir@gmail.com>
2023-09-28 00:07:19 +0545

    Footer

--- social_frontend/src/App.js
+++ social_frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import Header from "./component/layout/Header.js";
+import Header from "./component/layout/header/Header.js";
+import Footer from "./component/layout/footer/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <Router>
       <Header />
+      <Footer />
     </Router>
   );
 }
