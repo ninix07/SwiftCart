ff626ed39357ee4a2b6841dd1eed1befac08bbed
Nigam Niraula <nigam21nir@gmail.com>
2023-11-24 14:30:31 +0545

    Update Profile Image

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -141,6 +141,21 @@ exports.updateProfile = catchAsyncError(async (req, res, next) => {
     name: req.body.name,
     email: req.body.email,
   };
+
+  if (req.body.avatar !== "") {
+    const user = await User.findById(req.user.id);
+    const imageId = user.profile_image.public_id;
+    await cloudinary.v2.uploader.destroy(imageId);
+    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+      folder: "avatars",
+      width: 150,
+      crop: "scale",
+    });
+    newUserData.profile_image = {
+      public_id: myCloud.public_id,
+      url: myCloud.secure_url,
+    };
+  }
   const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
     new: true,
     runValidators: true,
