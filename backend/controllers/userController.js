2e3718ed608dc820766ae78e4e1ed03d3ce840fd
Nigam <nigam21nir@gmail.com>
2024-02-11 23:58:13 +0545

    Changes for Reset Password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -64,10 +64,10 @@ exports.forgotPassword = catchAsyncError(async (req, res, next) => {
   const resetToken = await user.getResetPasswordToken();
   await user.save({ validateBeforeSave: false });
   // console.log({ token });
-  const resetPasswordUrl = `${req.protocol}://${req.get(
-    "host"
-  )}/api/v1/password/reset/${resetToken}`;
-  const message = `Your password reset token is : \n\n
+  const resetPasswordUrl = 
+  // ${req.protocol}://${req.get(  "host" )/api/v1/}
+  `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
+  const message = `Your requested password reset token is : \n\n
   ${resetPasswordUrl}\n\n If you have not requested this email then please ignore it.`;
   const options = {
     email: user.email,
